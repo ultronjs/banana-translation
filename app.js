93c50645b34bce4f2df92523495fec8cc60759c4
ultron07 <50156573+ultron07@users.noreply.github.com>
2021-03-22 14:47:34 +0530

    Js done with real api

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var inputbox = document.querySelector("textarea");
 var btnTranslate = document.querySelector("#btn-translate");
-var outputBox = document.querySelector("#output");
+var outputBox = document.querySelector("#output"); 
+var testUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function urlToFetch(userInput){
+    return serverUrl + "?text=" + userInput;
+}
+
+function errorHandler(error){
+    console.log("Something is wrong with the server :(");
+    alert("Something is wrong with the server.Please try again later");
+}
 
 btnTranslate.addEventListener("click",function read(){
     var userInput = inputbox.value;
-    outputBox.innerText = "translated: " + userInput;
+    fetch(urlToFetch(userInput))
+    .then(response => response.json()
+    .then(json =>outputBox.innerHTML = json.contents.translated))
+    .catch(errorHandler)
 })
 
